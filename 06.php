30f43cee634a7bd920a2df40bdd3d57524c4d052
ly1314529 <1056510359@qq.com>
2016-09-10 00:28:04 -0400

    Create 06.php

---
+++ 06.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+	</head>
+	<body>
+    <?php
+    $fruit = "Apple";
+    
+    /* 
+    if () {
+    echo '';
+    }
+    else if () {
+    }
+    else {
+    }
+    */
+    
+   /*
+   switch ($fruit) {
+        case 1:
+        echo 'Apple';
+        break;
+        case 2:
+            echo "Yummy.";
+            break;
+            default:
+                echo "none";
+            
+    }
+    */
+    
+    /*
+    
+    
+    ?>
+    </body>
+</html>
